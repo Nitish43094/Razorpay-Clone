fefe3c2f20ddcea2d397450bf340e78dcb89005a
Nitish43094 <www.nitish93369@gmail.com>
2024-12-17 11:39:54 +0530

    Simple commit

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish :["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lihgtBlue:"2b84ea",
+        lihgtBlue300:"4b944ed",
+        lihgtBlue500:"0b72e7",
+        grayBlue:"#344a6c",
+        greenlight:"#61cea6",
+        grayText:"#818597",
+        lightGray:"#e2e2e2",
+        deepBlueHead:"#162f56",
+        gray2:"#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
